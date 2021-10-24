a1c92cc1d478a09adb122cf3877fc0231f8744ea
Della Arselatuiqrom <cikom.com@gmail.com>
2021-10-24 15:39:02 +0700

    commit final code

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,73 @@
+import 'package:converter/widget/convert.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:converter/widget/input.dart';
+import 'package:converter/widget/result.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-// This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // text controller
+  TextEditingController etInput = new TextEditingController();
+  //variabel berubah
+  double _inputUser = 0;
+  double _kelvin = 0;
+  double _reamur = 0;
+
+  _conversionTemperature() {
+    setState(() {
+      _inputUser = double.parse(etInput.text);
+      _reamur = 4 / 5 * _inputUser;
+      _kelvin = _inputUser + 273;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Converter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Konverter Suhu"),
+          title: Text("Temperature Converter"),
         ),
         body: Container(
           margin: EdgeInsets.all(8),
-          child: TextFormField(
-            decoration:
-                InputDecoration(hintText: "Masukkan Suhu Dalam Celcius"),
-          keyboardType: TextInputType.number,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Input(etInput: etInput),
+              Container(
+                margin: EdgeInsets.only(top: 20, bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Result(
+                      result: _kelvin,
+                      name: "Kelvin",
+                    ),
+                    Result(
+                      result: _reamur,
+                      name: "Reamur",
+                    ),
+                  ],
+                ),
+              ),
+              Convert(convertHandler: _conversionTemperature),
+            ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
